91dc57b1c39fc1ab2efca0934ca951ed1c025299
sujin Park <chemd78@naver.com>
2024-10-11 21:56:41 +0900

    :green_heart: chore: example 앱에 cbv, fbv URL 등록

--- chapter4/example/views.py
+++ chapter4/example/views.py
@@ -12,28 +12,28 @@ def HelloAPI(request):
     return Response("hello world!")
 
 
-# @api_view(['GET', 'POST'])
-# def booksAPI(request): # /book/
-#     if request.method == 'GET':
-#         books = Book.objects.all()
-#         # 시리얼라이저에 전체 데이터를 한 번에 집어넣기 (직렬화, many=True)
-#         serializer = BookSerializer(books, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#     elif request.method == 'POST':
-#         # POST 요청으로 들어온 데이터를 시리얼라이저에 집어넣기
-#         serializer = BookSerializer(data=request.data)
-#         if serializer.is_valid():
-#             # 시리얼라이저의 역직렬화를 통해 save(), 모델시리얼라이저의 기본 create() 함수가 동작
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'POST'])
+def booksAPI(request): # /book/
+    if request.method == 'GET':
+        books = Book.objects.all()
+        # 시리얼라이저에 전체 데이터를 한 번에 집어넣기 (직렬화, many=True)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        # POST 요청으로 들어온 데이터를 시리얼라이저에 집어넣기
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            # 시리얼라이저의 역직렬화를 통해 save(), 모델시리얼라이저의 기본 create() 함수가 동작
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['GET'])
-# def bookAPI(request, bid): # /book/bid/
-#     book = get_object_or_404(Book, bid=bid)
-#     serializer = BookSerializer(book) # 시리얼라이저에 데이터 집어넣기 (직렬화)
-#     return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def bookAPI(request, bid): # /book/bid/
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializer(book) # 시리얼라이저에 데이터 집어넣기 (직렬화)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class BooksAPI(APIView):
